fb1bb8c9b847fd72335571e95fabb69d7b2af3b6
Matheus Almeida Bastos <matheus.mab432@gmail.com>
2023-07-14 07:42:20 -0300

    feat(be): adding create poll feature

--- backend/query/src/post.rs
+++ backend/query/src/post.rs
@@ -3,7 +3,8 @@ use diesel::prelude::*;
 use diesel::{PgConnection, RunQueryDsl};
 
 use serde::{Deserialize, Serialize};
-use uchat_domain::ids::{PostId, UserId};
+use uchat_domain::ids::{PollChoiceId, PostId, UserId};
+use uchat_endpoint::post::types::VoteCast;
 
 use crate::{schema, DieselError};
 
@@ -42,10 +43,29 @@ impl Post {
 
 pub fn new(conn: &mut PgConnection, post: Post) -> Result<PostId, DieselError> {
     conn.transaction::<PostId, DieselError, _>(|conn| {
+        use uchat_endpoint::post::types::Content as EndpointContent;
+
         diesel::insert_into(schema::posts::table)
             .values(&post)
             .execute(conn)?;
-        Ok(post.id)
+
+        match serde_json::from_value::<EndpointContent>(post.content.0) {
+            Ok(EndpointContent::Poll(poll)) => {
+                for choice in &poll.choices {
+                    use schema::poll_choices::{self, columns as col};
+
+                    diesel::insert_into(poll_choices::table)
+                        .values((
+                            col::id.eq(choice.id),
+                            col::choice.eq(choice.description.as_ref()),
+                            col::post_id.eq(post.id),
+                        ))
+                        .execute(conn)?;
+                }
+                Ok(post.id)
+            }
+            _ => Ok(post.id),
+        }
     })
 }
 
@@ -298,5 +318,82 @@ pub fn delete_boost(
             .filter(user_id.eq(uid))
             .execute(conn)
             .map(DeleteStatus::new)
+    }
+}
+
+pub fn vote(
+    conn: &mut PgConnection,
+    user_id: UserId,
+    post_id: PostId,
+    choice_id: PollChoiceId,
+) -> Result<VoteCast, DieselError> {
+    let uid = user_id;
+    let pid = post_id;
+    let cid = choice_id;
+
+    {
+        use crate::schema::poll_votes::dsl::*;
+
+        diesel::insert_into(poll_votes)
+            .values((user_id.eq(uid), post_id.eq(pid), choice_id.eq(cid)))
+            .on_conflict((user_id, post_id))
+            .do_nothing()
+            .execute(conn)
+            .map(|n| {
+                if n == 1 {
+                    VoteCast::Yes
+                } else {
+                    VoteCast::AlreadyVoted
+                }
+            })
+    }
+}
+
+pub fn did_vote(
+    conn: &mut PgConnection,
+    user_id: UserId,
+    post_id: PostId,
+) -> Result<Option<PollChoiceId>, DieselError> {
+    let uid = user_id;
+    let pid = post_id;
+
+    {
+        use crate::schema::poll_votes::dsl::*;
+
+        poll_votes
+            .filter(post_id.eq(pid))
+            .filter(user_id.eq(uid))
+            .select(choice_id)
+            .get_result(conn)
+            .optional()
+    }
+}
+
+pub struct PollResults {
+    pub post_id: PostId,
+    pub results: Vec<(PollChoiceId, i64)>,
+}
+
+pub fn get_poll_results(
+    conn: &mut PgConnection,
+    post_id: PostId,
+) -> Result<PollResults, DieselError> {
+    let pid = post_id;
+
+    {
+        use crate::schema::poll_votes::dsl::*;
+
+        use diesel::dsl::count;
+
+        let results = poll_votes
+            .filter(post_id.eq(pid))
+            .group_by(choice_id)
+            .select((choice_id, count(choice_id)))
+            .load::<(PollChoiceId, i64)>(conn)?;
+
+        Ok(PollResults {
+            post_id: pid,
+            results,
+        })
     }
 }
---
+++ frontend/src/page/new_post/poll.rs
@@ -0,0 +1,150 @@
+#![allow(non_snake_case)]
+
+use std::collections::HashMap;
+
+use crate::{
+    fetch_json,
+    prelude::*,
+    toasty,
+    util::{self},
+};
+use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
+use uchat_domain::post::{PollChoiceDescription, PollHeadline};
+use uchat_endpoint::post::types::NewPostOptions;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PageState {
+    pub headline: String,
+    pub poll_choices: HashMap<usize, String>,
+    pub next_id: usize,
+}
+
+impl Default for PageState {
+    fn default() -> Self {
+        Self {
+            headline: "".to_owned(),
+            poll_choices: {
+                let mut map = HashMap::new();
+                map.insert(0, "".to_owned());
+                map.insert(1, "".to_owned());
+                map
+            },
+            next_id: 2,
+        }
+    }
+}
+
+impl PageState {
+    // TODO refactor common can_submit pattern to be more concise
+    pub fn can_submit(&self) -> bool {
+        if PollHeadline::new(&self.headline).is_err() {
+            return false;
+        }
+
+        if self.poll_choices.len() < 2 {
+            return false;
+        }
+
+        if self
+            .poll_choices
+            .values()
+            .map(PollChoiceDescription::new)
+            .collect::<Result<Vec<PollChoiceDescription>, _>>()
+            .is_err()
+        {
+            return false;
+        }
+
+        true
+    }
+
+    pub fn push_choice<T: Into<String>>(&mut self, choice: T) {
+        self.poll_choices.insert(self.next_id, choice.into());
+        self.next_id += 1;
+    }
+
+    pub fn replace_choice<T: Into<String>>(&mut self, key: usize, choice: T) {
+        self.poll_choices.insert(key, choice.into());
+    }
+}
+
+#[inline_props]
+// TODO refactor to text input
+pub fn HeadlineInput(cx: Scope, page_state: UseRef<PageState>) -> Element {
+    use uchat_domain::post::Headline;
+
+    let max_chars = Headline::MAX_CHARS;
+    let wrong_len = maybe_class!(
+        "err-text-color",
+        page_state.read().headline.len() > max_chars || page_state.read().headline.is_empty()
+    );
+
+    cx.render(rsx! {
+        div {
+            label { r#for: "headline",
+                div { class: "flex flex-row justify-between",
+                    span { "Headline" }
+                    span { class: "text-right {wrong_len}", "{page_state.read().headline.len()}/{max_chars}" }
+                }
+            }
+            input {
+                class: "input-field",
+                id: "headline",
+                value: "{page_state.read().headline}",
+                oninput: move |ev| {
+                    page_state.with_mut(|state| state.headline = ev.data.value.clone());
+                }
+            }
+        }
+    })
+}
+
+pub fn NewPoll(cx: Scope) -> Element {
+    let page_state = use_ref(cx, PageState::default);
+    let is_invalid = !page_state.read().can_submit();
+    let toaster = use_toaster(cx);
+    let router = use_router(cx);
+    let api_client = ApiClient::global();
+
+    let form_onsubmit = async_handler!(
+        &cx,
+        [toaster, api_client, page_state, router],
+        move |_| async move {
+            use uchat_domain::post::PollHeadline;
+            use uchat_endpoint::post::endpoint::{NewPost, NewPostOk};
+            use uchat_endpoint::post::types::Poll;
+
+            let request = NewPost {
+                content: Poll {
+                    headline: todo!(),
+                    choices: todo!(),
+                    voted: todo!(),
+                }
+                .into(),
+                options: NewPostOptions::default(),
+            };
+
+            let response = fetch_json!(<NewPostOk>, api_client, request);
+            match response {
+                Ok(_) => {
+                    router.replace_route(page::HOME, None, None);
+                    toasty!(toaster => success: "new post created!", 3);
+                }
+                Err(e) => {
+                    toasty!(toaster => error: format!("Post failed: {e}"));
+                }
+            }
+        }
+    );
+
+    let submit_btn_style = maybe_class!("btn-disabled", is_invalid);
+
+    cx.render(rsx! {
+        form { class: "flex flex-col gap-4", onsubmit: form_onsubmit, prevent_default: "onsubmit",
+        HeadlineInput { page_state: page_state.clone() }
+        // poll choices
+        button { class: "btn {submit_btn_style}", r#type: "submit", disabled: is_invalid, "Post" }
+        }
+    })
+}
